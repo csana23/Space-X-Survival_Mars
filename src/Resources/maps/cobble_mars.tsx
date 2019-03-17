77d01bbefb5074ff720ddcfa73f13aa4d2b7ce38
Richard Csanaki <richard.csanaki@gmail.com>
2019-03-17 18:06:43 +0100

    Add files via upload

---
+++ src/Resources/maps/cobble_mars.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cobble_mars" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <terraintypes>
+  <terrain name="Cobble" tile="0"/>
+ </terraintypes>
+ <tile id="0">
+  <image width="32" height="32" source="../../crawl-tiles Oct-5-2010/crawl-tiles Oct-5-2010/dc-dngn/floor/cobble_blood1.png"/>
+ </tile>
+</tileset>
